18401608eee1fed9fd2778df7f48048f684cf3b3
Denis Rodionov <nicholas.hawkwood@gmail.com>
2023-03-06 20:44:02 +0200

    add lint and format

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,19 +2,16 @@ import { Hero, Button } from 'react-daisyui'
 
 function App() {
   return (
-      <Hero className="min-h-screen">
-          <Hero.Overlay className="bg-opacity-60" />
-          <Hero.Content className="text-center">
-              <div className="max-w-md">
-                  <h1 className="text-5xl font-bold">Opus Classical</h1>
-                  <p className="py-6">
-                      Admin panel
-                  </p>
-
-                  <Button color="primary">Get Started</Button>
-              </div>
-          </Hero.Content>
-      </Hero>
+    <Hero className='min-h-screen'>
+      <Hero.Overlay className='bg-opacity-60' />
+      <Hero.Content className='text-center'>
+        <div className='max-w-md'>
+          <h1 className='text-5xl font-bold'>Opus Classical</h1>
+          <p className='py-6'>Admin panel</p>
+          <Button color='primary'>Get Started</Button>
+        </div>
+      </Hero.Content>
+    </Hero>
   )
 }
 
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App'
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <GoogleOAuthProvider clientId={import.meta.env.OAUTH_CLIENT_ID}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>,
 )

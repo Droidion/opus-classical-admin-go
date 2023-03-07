a11c6e5a5149a7cd86e973c7471216133e934a0d
Denis Rodionov <denis.rodionov@aibidia.com>
2023-03-07 16:36:51 +0200

    add router

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { RouterProvider } from "react-router-dom";
 import { RecoilRoot } from "recoil";
-import App from "./App";
 import "./index.scss";
-import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { router } from "./router";
 
 const queryClient = new QueryClient();
 
@@ -13,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <QueryClientProvider client={queryClient}>
       <React.StrictMode>
         <RecoilRoot>
-          <App />
+          <RouterProvider router={router} />
         </RecoilRoot>
       </React.StrictMode>
     </QueryClientProvider>
